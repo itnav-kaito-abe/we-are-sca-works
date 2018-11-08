0abfee5a8a2d509a9bdf1266b80af81163e32e03
kaito027 <kaito.san0.1818@gmail.com>
2018-11-08 17:07:11 +0900

    add-SceneCenge&FPS

---
+++ Assets/Script/SceneCenge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCenge : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GameStart() {
+        SceneManager.LoadScene("Game");
+    }
+}
