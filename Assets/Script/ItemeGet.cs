655c19a01e704f0eb9fdcf64ac04792857b560b2
kaito <kaito.san0.1818@gmail.com>
2018-11-16 17:39:32 +0900

    add-ItemGet

---
+++ Assets/Script/ItemeGet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemeGet : MonoBehaviour {
+
+    public GameObject OperationKeyImage;
+
+	// Use this for initialization
+	void Start () {
+	    OperationKeyImage = GameObject.Find("Canvas").gameObject.transform.Find("OperationKeyImage").gameObject;
+        OperationKeyImage.SetActive(false);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "key") {
+            OperationKeyImage = GameObject.Find("Canvas").gameObject.transform.Find("OperationKeyImage").gameObject;
+
+            OperationKeyImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Syutoku");
+
+            OperationKeyImage.GetComponent<Image>().preserveAspect = true;
+
+            OperationKeyImage.GetComponent<Image>().SetNativeSize();
+
+            OperationKeyImage.SetActive(true);
+        }
+    }
+
+    public void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "key") {
+            GameObject key = GameObject.Find("key");
+
+            if (Input.GetKeyDown(KeyCode.H)) {
+                Destroy(key);
+                OperationKeyImage.SetActive(false);
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        OperationKeyImage = GameObject.Find("Canvas").gameObject.transform.Find("OperationKeyImage").gameObject;
+        OperationKeyImage.SetActive(false);
+    }
+}
